229ffd1e51b7c9eea89f99325ab6c61f8f7073be
alirezadalvand <alireza.d1339@gmail.com>
2021-04-04 19:39:03 +0430

    opps added

---
+++ app.js
@@ -0,0 +1,13 @@
+let array1 = ["ayda", " hanie"];
+
+let array2 = array1;
+
+console.log(array1);
+console.log(array2);
+
+// console.log(array1 === array2);
+
+// let str1 = "ayda";
+// let str2 = "ayda";
+
+// console.log(str1 === str2);
